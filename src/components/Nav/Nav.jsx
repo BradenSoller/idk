80d8556562f1f5d569e9c3168dc2de56af5b5145
Braden Soller <braden.soller18@gmail.com>
2024-06-07 13:55:38 -0500

    changed ports and nav styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title"></h2>
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
@@ -29,16 +29,20 @@ function Nav() {
             </Link>
 
             <Link className="navLink" to="/info">
-              Info Page
+              Favorites 
+            </Link>
+
+            <Link className="navLink" to="/about">
+              About
             </Link>
 
             <LogOutButton className="navLink" />
           </>
         )}
 
-        <Link className="navLink" to="/about">
+        {!user.id && ( <Link className="navLink" to="/about">
           About
-        </Link>
+        </Link>)}
       </div>
     </div>
   );
