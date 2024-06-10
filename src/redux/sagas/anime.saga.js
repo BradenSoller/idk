74e0de3767ab29004824b8e1673591af7646a84b
Braden Soller <braden.soller18@gmail.com>
2024-06-10 11:49:51 -0500

    added anime "get" saga

---
+++ src/redux/sagas/anime.saga.js
@@ -0,0 +1,18 @@
+import { put, take, takeLatest } from "redux-saga/effects";
+import axios from "axios";
+
+ function* getAllAnime() {
+        try {
+          const response = yield axios({
+            method: "GET",
+            url: "/api/anime",
+          });
+          yield put({
+            type: "SET_ANIME",
+            payload: response.data,
+          });
+        } catch (error) {
+          console.log("Unable to get pending events from server", error);
+        }
+
+ }
