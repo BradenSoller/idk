1a1bfd17b6bd2b3ad355815c0135b3a0efbd8605
Braden Soller <braden.soller18@gmail.com>
2024-06-10 12:07:40 -0500

    added use selector to home page to grab the info from the anime reducer

--- src/components/UserPage/Home.jsx
+++ src/components/UserPage/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux'; 
+import { useEffect, useState } from "react";
+import { useDispatch } from 'react-redux';
+
 
 function HomePage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
+
+  const Animes = useSelector ((store) => store.AllAnime) // store connects to reducers 
+
   return (
     <div className="container">
   
