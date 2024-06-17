8df15484baf708b26497da7b097508198f28d9c7
Braden Soller <braden.soller18@gmail.com>
2024-06-17 13:07:40 -0500

    created reducer to hold new anime's

---
+++ src/redux/reducers/newAnime.reducer.js
@@ -0,0 +1,12 @@
+const newAnime = (state = [], action) => {
+
+    switch (action.type) {
+        case 'NEW_ANIME':
+            return action.payload
+
+        default:
+            return state;
+    }
+}
+
+export default newAnime
