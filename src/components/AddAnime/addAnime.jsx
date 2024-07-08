a623dae36b9a2d0296b8e1561a85aa900fb34114
Braden Soller <braden.soller18@gmail.com>
2024-07-08 13:28:24 -0500

    created new file for anime adding

---
+++ src/components/AddAnime/addAnime.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, Fragment } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import './ScheduleForm.css';
+import { Button } from "@mui/material";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function AddAnime() {
+    
+}
