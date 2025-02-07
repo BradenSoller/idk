66c4ad22fa3f9c21591851c94e7e034491ea0a01
Braden Soller <braden.soller18@gmail.com>
2025-02-07 11:36:18 -0600

    blurred images  in modal and changed modal layout

--- src/components/UserPage/Home.jsx
+++ src/components/UserPage/Home.jsx
@@ -1,60 +1,56 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import { useSelector } from 'react-redux';
+import React from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import "./Home.css";
 import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
 import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
-import { Button } from '@mui/material';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Button } from "@mui/material";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
-import { Modal } from '@mui/material';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Rating from '@mui/material/Rating';
-import StarIcon from '@mui/icons-material/Star';
-
+import { Modal } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import StarIcon from "@mui/icons-material/Star";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 1000,
   height: 500,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
   borderRadius: 5,
 };
 
 const labels = {
-  0.5: 'Useless',
-  1: 'Useless+',
-  1.5: 'Poor',
-  2: 'Poor+',
-  2.5: 'Ok',
-  3: 'Ok+',
-  3.5: 'Good',
-  4: 'Good+',
-  4.5: 'Excellent',
-  5: 'Excellent+',
+  0.5: "Useless",
+  1: "Useless+",
+  1.5: "Poor",
+  2: "Poor+",
+  2.5: "Ok",
+  3: "Ok+",
+  3.5: "Good",
+  4: "Good+",
+  4.5: "Excellent",
+  5: "Excellent+",
 };
 
-
 function getLabelText(value) {
-  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
 }
 
-
 function HomePage() {
-
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
-  const Animes = useSelector((store) => store.AllAnime)
+  const Animes = useSelector((store) => store.AllAnime);
 
   console.log("all animes", Animes);
 
@@ -62,8 +58,7 @@ function HomePage() {
 
   console.log("singleAnime", singleAnime);
 
-
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
   const [imageInput, setImageInput] = useState("");
 
@@ -74,9 +69,8 @@ function HomePage() {
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => {
-
     setOpen(true);
-  }
+  };
 
   const handleClose = () => setOpen(false);
 
@@ -85,26 +79,21 @@ function HomePage() {
     window.scrollTo(0, 0);
   }, []);
 
-
-
-  const history = useHistory()
+  const history = useHistory();
   const eventForm = new FormData();
 
-
   const newAnime = (event) => {
-
     event.preventDefault();
     eventForm.append("image", imageInput);
     eventForm.append("title", title);
 
     dispatch({
       type: "FETCH_NEW_ANIME",
-      payload: eventForm
-    }
-    );
-    setTitle('')
-    setImageInput('')
-  }
+      payload: eventForm,
+    });
+    setTitle("");
+    setImageInput("");
+  };
 
   const StatusChange = (id) => {
     dispatch({
@@ -124,7 +113,7 @@ function HomePage() {
     console.log("payload", id);
     dispatch({
       type: "FETCH_SELECTED_ANIME",
-      payload: { id }
+      payload: { id },
     });
   };
 
@@ -134,7 +123,9 @@ function HomePage() {
     <div className="container">
       <div>
         <div>
-          <input className='inputAnime' type="text"
+          <input
+            className="inputAnime"
+            type="text"
             value={title}
             onChange={(event) => setTitle(event.target.value)}
           />
@@ -150,15 +141,25 @@ function HomePage() {
           />
         </div>
         <div>
-          <button className='submitAnime' onClick={newAnime}>submit</button>
+          <button className="submitAnime" onClick={newAnime}>
+            submit
+          </button>
         </div>
       </div>
-      <div className='backroundCard'>
-
+      <div className="backroundCard">
         {Animes.map((anime) => {
           return (
-            <div className="animeCards" key={anime.id} onClick={() => FetchSingleAnime(anime.id)}>
-              <img onClick={handleOpen} className="cardImage" src={anime.image} alt={anime.title} />
+            <div
+              className="animeCards"
+              key={anime.id}
+              onClick={() => FetchSingleAnime(anime.id)}
+            >
+              <img
+                onClick={handleOpen}
+                className="cardImage"
+                src={anime.image}
+                alt={anime.title}
+              />
               {/* Like/Unlike Button */}
               <Button className="star" onClick={() => StatusChange(anime.id)}>
                 {anime.is_liked ? (
@@ -168,14 +169,16 @@ function HomePage() {
                 )}
               </Button>
               {/* Delete Button */}
-              <Button className="deleteAnime" onClick={() => deleteAnime(anime)}>
+              <Button
+                className="deleteAnime"
+                onClick={() => deleteAnime(anime)}
+              >
                 <DeleteOutlineOutlinedIcon />
               </Button>
             </div>
-          )
-        }
-        )}
-        <div className='ModalHome'>
+          );
+        })}
+        <div className="ModalHome">
           <Modal
             open={open}
             onClose={handleClose}
@@ -183,16 +186,31 @@ function HomePage() {
             aria-describedby="modal-modal-description"
           >
             <Box sx={style}>
-              <Typography id="modal-modal-title" variant="h6" component="h2">
-                Text in a modal
-              </Typography>
               <div>
                 {singleAnime.map((anime) => {
                   return (
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                    <Typography
+                      id="modal-modal-title"
+                      variant="h6"
+                      component="h2"
+                    >
                       {anime.title}
-                      <img className='backgroundImage' src={anime.image} alt="" />
-                      <Box sx={{ width: 200, display: 'flex', alignItems: 'center' }}>
+                      <img
+                        className="backgroundImage"
+                        src={anime.image}
+                        alt=""
+                      />
+
+                      <br />
+                      <br />
+
+                      <Box
+                        sx={{
+                          width: 200,
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
                         <Rating
                           name="hover-feedback"
                           value={value}
@@ -204,15 +222,21 @@ function HomePage() {
                           onChangeActive={(event, newHover) => {
                             setHover(newHover);
                           }}
-                          emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                          emptyIcon={
+                            <StarIcon
+                              style={{ opacity: 0.55 }}
+                              fontSize="inherit"
+                            />
+                          }
                         />
                         {value !== null && (
-                          <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+                          <Box sx={{ ml: 2 }}>
+                            {labels[hover !== -1 ? hover : value]}
+                          </Box>
                         )}
                       </Box>
                     </Typography>
-                    
-                  )
+                  );
                 })}
               </div>
             </Box>
